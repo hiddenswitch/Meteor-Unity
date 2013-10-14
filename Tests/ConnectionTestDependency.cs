13a315017a10077ebf293356c1cfaf4a9b9b9227
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2013-10-13 17:11:14 -0700

    Fixing connection tests.

--- Tests/ConnectionTestDependency.cs
+++ Tests/ConnectionTestDependency.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEngine;
 
 namespace Meteor.Tests
 {
@@ -10,13 +14,48 @@ namespace Meteor.Tests
 		{
 		}
 
-		[TestSetup]
+		bool meteorStarted;
+
+		[Test]
 		public IEnumerator ConnectToLocalhost() {
-			if (!Client.IsConnected) {
-				yield return Client.Connect ("ws://127.0.0.1:3000/websocket");
+//			// Start meteor if it isn't running
+//			meteorStarted = Process.GetProcesses ().Count (p => {
+//				try {
+//					return p.ProcessName == "node";
+//				} catch (Exception e) {
+//					return false;
+//				}
+//			}) != 0;
+//
+//			if (!meteorStarted) {
+//				var meteorPath = Directory.GetParent (Application.dataPath).GetDirectories ("Meteor") [0].FullName;
+//
+//				var process = new Process () {
+//					StartInfo = new ProcessStartInfo () {
+//						WorkingDirectory = meteorPath,
+//						FileName = "/usr/local/share/npm/bin/mrt",
+//						UseShellExecute = false,
+//						RedirectStandardOutput = true
+//					},
+//					EnableRaisingEvents = true
+//				};
+//
+//				process.OutputDataReceived += (object sender, DataReceivedEventArgs e) => {
+//					if (e.Data.Contains("Meteor server running")) {
+//						meteorStarted = true;
+//					}
+//				};
+//
+//				process.Start ();
+//				process.BeginOutputReadLine ();
+//				UnityEngine.Debug.Log(process.Threads.Count ());
+//			}
+
+			if (!LiveData.Instance.Connected) {
+				yield return LiveData.Instance.Connect ("ws://127.0.0.1:3000/websocket");
 			}
 
-			Assert.IsTrue (Client.IsConnected);
+			Assert.IsTrue (LiveData.Instance.Connected);
 
 			yield break;
 		}
