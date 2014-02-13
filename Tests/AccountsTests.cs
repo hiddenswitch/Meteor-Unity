268773fe6a04ff9f58a87ef97a4ab7462a735916
Benjamin Berman <benjamin.s.berman@gmail.com>
2014-02-13 10:51:34 -0800

    Update AccountsTests.cs

--- Tests/AccountsTests.cs
+++ Tests/AccountsTests.cs
@@ -9,21 +9,22 @@ namespace Meteor.Tests
 	[TestFixture]
 	public class AccountsTests : ConnectionTestDependency
 	{
+		// Make sure that the accounts-password package is added to your meteor server
 		[Test]
 		public IEnumerator LoginAsGuest() {
 			yield return Accounts.LoginAsGuest ();
 			Assert.IsNotNull (Accounts.UserId);
 			yield break;
 		}
-//
-//		[Test]
-//		public IEnumerator CreateAndLoginUser() {
-//			yield return Accounts.CreateAndLoginWith (string.Format ("test{0}@gtest.com", UnityEngine.Random.Range (0, 100000000)),
-//			                                          string.Format ("test{0}", UnityEngine.Random.Range (0, 100000000)),
-//			                                          "testpassword");
-//			Assert.IsNotNull (Accounts.UserId);
-//			yield break;
-//		}
+
+		[Test]
+		public IEnumerator CreateAndLoginUser() {
+			yield return Accounts.CreateAndLoginWith (string.Format ("test{0}@gtest.com", UnityEngine.Random.Range (0, 100000000)),
+			                                          string.Format ("test{0}", UnityEngine.Random.Range (0, 100000000)),
+			                                          "testpassword");
+			Assert.IsNotNull (Accounts.UserId);
+			yield break;
+		}
 	}
 }
 #endif
