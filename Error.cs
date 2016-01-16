09e7a567fd51774ab9ae2147e863ec29d28ee570
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-01-16 15:58:25 -0500

    Make sure null comparisons with Error take into account that Serializable classes in Unity get filled with empty classes

--- Error.cs
+++ Error.cs
@@ -7,7 +7,48 @@ namespace Meteor
 		public int error;
 		public string details;
 
-		public Error() {}
+		public Error ()
+		{
+		}
+
+		bool IsNull ()
+		{
+			return error == 0
+			&& string.IsNullOrEmpty (reason)
+			&& string.IsNullOrEmpty (details);
+		}
+
+		public override int GetHashCode ()
+		{
+			return base.GetHashCode ();
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (obj == null) {
+				return IsNull ();
+			}
+
+			var other = (Error)obj;
+
+			return reason == other.reason
+			&& error == other.error;
+		}
+
+		public static bool operator == (Error a, Error b)
+		{
+			var isNullA = (object)a == null || a.IsNull ();
+			var isNullB = (object)b == null || b.IsNull ();
+
+			var nullComparison = isNullA && isNullB;
+
+			return nullComparison || (isNullA ? b.Equals (a) : a.Equals (b));
+		}
+
+		public static bool operator != (Error a, Error b)
+		{
+			return !(a == b);
+		}
 	}
 }
 
