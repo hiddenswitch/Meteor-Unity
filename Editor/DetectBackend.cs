52a82afe48fbd3bc7862008fa7f220cbde126d7f
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-03-03 12:33:58 -0500

    Add warning about IL2CPP

---
+++ Editor/DetectBackend.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Meteor.Editor
+{
+	[InitializeOnLoad]
+	public class DetectBackend
+	{
+		static DetectBackend ()
+		{
+			#if ENABLE_IL2CPP
+			Debug.LogError ("Meteor-Unity is currently not supported on the IL2CPP backend.");
+			#endif
+		}
+	}
+}
