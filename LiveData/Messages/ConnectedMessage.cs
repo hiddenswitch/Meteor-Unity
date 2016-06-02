ad023e10b8256cfdf99711e0cf5ae460c2ecc5b0
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-06-02 10:53:30 -0400

    Disabling spurious warnings

--- LiveData/Messages/ConnectedMessage.cs
+++ LiveData/Messages/ConnectedMessage.cs
@@ -5,7 +5,11 @@ namespace Meteor.Internal
 	internal class ConnectedMessage : Message
 	{
 		public const string connected = "connected";
+		// Disabling the warning here because fields is assigned to, it's just assigned to via reflection
+		// so the compiler doesn't know
+		#pragma warning disable 0649 
 		public string session;
+		#pragma warning restore 0649
 
 		public ConnectedMessage()
 		{
